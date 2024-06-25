c3735b5ce579638219f276a90426e98f0e67e51d
readable-ko <21700026@handong.edu>
2024-06-25 18:48:45 +0900

    feat(api): api controller 구현
    api 정보값을 받아오기위한 controller 구현
    - _get_paginated_data: 페이징된 api값을 차례로 호출
    - get_org_api: 각 기관에 속한 유저 목록 api 호출
    - get_user_api: 각 유저에 대한 정보 api 호출

---
+++ test/test_api_controller.py
@@ -0,0 +1,62 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from server.api_controller import ApiController
+
+
+class TestApiController(unittest.TestCase):
+    @patch('server.utility.Utility.load_config')
+    def setUp(self, mock_load_config):
+        self.mock_config = {
+            "api": {
+                "ORG_API_URL": "https://example.com/org",
+                "USER_API_URL": "https://example.com/user",
+                "TARGET": "lists",
+                "COUNT": "total_count"
+            }
+        }
+
+        mock_load_config.return_value = self.mock_config
+
+        # Mocked response from requests.get
+        self.api = ApiController()
+        self.api.init()
+
+    @patch('requests.get')
+    def test_get_org_api(self, mock_requests_get):
+        self.mock_response = MagicMock()
+        self.mock_response.status_code = 200
+        self.mock_response.json.return_value = {
+            'lists': [{"id": 1, 'name': "eric"}],
+            'total_count': 50
+        }
+        mock_requests_get.return_value = self.mock_response
+        result = self.api.get_org_api()
+        self.assertEqual(result, [{'id': 1, 'name': "eric"}])
+
+    @patch('requests.get')
+    def test_get_user_api_fail_with_requests(self, mock_requests_get):
+        self.mock_response = MagicMock()
+        self.mock_response.status_code = 500
+        self.mock_response.json.return_value = {
+            'lists': [{"id": 1, 'name': "eric"}],
+            'total_count': 50
+        }
+        mock_requests_get.return_value = self.mock_response
+        result = self.api.get_user_api("test")
+        self.assertEqual(result, [])
+
+    @patch('server.api_controller._send_request')
+    def test_get_user_api_with_send_request(self, mock_send_request):
+
+        self.mock_response = {
+            'lists': [{"id": 1, 'name': "eric"}],
+            'total_count': 50
+        }
+        mock_send_request.return_value = self.mock_response
+        result = self.api.get_user_api("test")
+        self.assertEqual(result, [{'id': 1, 'name': "eric"}])
+        mock_send_request.assert_called_once_with('https://example.com/user', 1, 'test')
+
+
+if __name__ == '__main__':
+    unittest.main()
