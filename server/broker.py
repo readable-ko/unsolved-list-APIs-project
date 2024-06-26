296b2aa5579b91e40a5f5331c77be44a9198dd6d
readable-ko <21700026@handong.edu>
2024-06-26 17:07:18 +0900

    feat(broker): 브로커 모듈 개발
    - init(): rabbitMQ와 Data transformer 호출 및 초기화
    - send(): 큐에 갱신될 유저명과 유저번호를 넣음

---
+++ server/broker.py
@@ -0,0 +1,31 @@
+from server.data_transformer import DataTransformer
+from server.rabbit_mq_controller import RabbitMQController
+
+
+class Broker:
+    def __init__(self, config_path='config_json'):
+        self.db_users = []
+        self.api_users = []
+        self.mq_con = RabbitMQController(config_path)
+        self.transformer = DataTransformer(config_path)
+
+    def init(self):
+        self.mq_con.init()
+        self.transformer.init()
+
+    def close(self):
+        self.mq_con.end_connection()
+        self.transformer.close()
+
+    def send(self):
+        update_user = self.transformer.choose_user()
+        for user in update_user:
+            message = {user[0]: user[1]}
+            self.mq_con.push_queue(message)
+
+
+if __name__ == '__main__':
+    broker = Broker()
+    broker.init()
+    broker.send()
+    broker.close()
