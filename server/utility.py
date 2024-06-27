cb0b4f4c21143bba99b0e10ebed7842a4bd7b23c
readable-ko <21700026@handong.edu>
2024-06-27 19:24:24 +0900

    fix(json bug): json 파일과 string 분리
    수정 사항
    - @Utility - check_none(): is_none_or_empty()로 이름 직관적으로 변경
    - @Utility - load_json(): text를 json형식으로 변경하도록 추가
    - @Utility - load_config(): config_path에 있는 json 파일을 읽어오도록 변경

--- server/utility.py
+++ server/utility.py
@@ -3,12 +3,16 @@ import json
 
 class Utility:
     @staticmethod
+    def load_json(config_file):
+        return json.loads(config_file)
+
+    @staticmethod
     def load_config(config_path):
         with open(config_path) as config_file:
             return json.load(config_file)
 
     @staticmethod
-    def check_none(value):
+    def is_none_or_empty(value):
         if not value:
             return True
         return False
