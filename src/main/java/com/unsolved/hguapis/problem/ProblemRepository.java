f8a8ef0cdd638ce577d9d921f72644e4e27ac6fd
readable-ko <21700026@handong.edu>
2024-07-03 00:39:54 +0900

    feat(Problem): 검색 기능 지원
    주요 변경 사항:
    Problem
    - 기존 모든 리스트 태그, 제목, 번호 검색 기능 추가
    
    특이사항
    -test 미개발

--- src/main/java/com/unsolved/hguapis/problem/ProblemRepository.java
+++ src/main/java/com/unsolved/hguapis/problem/ProblemRepository.java
@@ -2,16 +2,10 @@ package com.unsolved.hguapis.problem;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
-    //    select * from problem
-//    WHERE problem.id not in (SELECT DISTINCT problem_solved_id FROM user_info_problem_solved)
-    @Query("SELECT p "
-            + "FROM Problem p "
-            + "WHERE p.id NOT IN "
-            + "(SELECT DISTINCT up.id.problemSolvedId "
-            + "FROM UserInfoProblemSolved up) ")
-    Page<Problem> findUnsolvedProblems(Pageable pageable);
+
+    Page<Problem> findAll(Specification<ProblemDto> spec, Pageable pageable);
 }
