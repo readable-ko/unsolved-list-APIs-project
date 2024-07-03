f6cc409e6067256608159eed2caeae129400b372
readable-ko <21700026@handong.edu>
2024-07-04 02:22:26 +0900

    feat(Question): create question page
    주요 변경 사항:
    Question: make board form for create question

--- src/main/java/com/unsolved/hguapis/question/QuestionController.java
+++ src/main/java/com/unsolved/hguapis/question/QuestionController.java
@@ -1,11 +1,15 @@
 package com.unsolved.hguapis.question;
 
+import com.unsolved.hguapis.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,9 +30,23 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(@PathVariable("id") Integer id, Model model) {
+    public String detail(@PathVariable("id") Integer id, Model model, AnswerForm answerForm) {
         Question question = this.questionService.getQuestionById(id);
         model.addAttribute("question", question);
         return "board_detail";
+    }
+
+    @GetMapping("/create")
+    public String createAnswer(QuestionForm questionForm) {
+        return "board_form";
+    }
+
+    @PostMapping("/create")
+    public String createQuestion(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "board_form";
+        }
+//        this.questionService.createQuestion(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/board";
     }
 }
---
+++ src/main/java/com/unsolved/hguapis/question/QuestionForm.java
@@ -0,0 +1,20 @@
+package com.unsolved.hguapis.question;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuestionForm {
+    @NotEmpty(message = "제목은 필수항목입니다.")
+    @Size(max = 200)
+    private String subject;
+
+    @NotEmpty(message = "내용은 필수항목입니다.")
+    private String content;
+}
