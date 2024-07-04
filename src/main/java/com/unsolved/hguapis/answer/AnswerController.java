c19381448b74357281d9912ff2b0a5e79f3afafd
readable-ko <21700026@handong.edu>
2024-07-04 21:37:16 +0900

    feat(Answer): create function for update, delete, vote Answer
    주요 변경 사항:
    Answer: Entity생성
    AnswerController: CRUD mapping
    AnswerForm: Answer작성 유무 확인
    AnswerRepository: Repository 연결
    AnswerService: 서비스

---
+++ src/main/java/com/unsolved/hguapis/answer/AnswerController.java
@@ -0,0 +1,95 @@
+package com.unsolved.hguapis.answer;
+
+import com.unsolved.hguapis.question.Question;
+import com.unsolved.hguapis.question.QuestionService;
+import com.unsolved.hguapis.siteUser.SiteUser;
+import com.unsolved.hguapis.siteUser.SiteUserService;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+@Controller
+public class AnswerController {
+    private final AnswerService answerService;
+    private final QuestionService questionService;
+    private final SiteUserService siteUserService;
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create/{id}")
+    public String createAnswer(@PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult,
+                               Principal principal, Model model) {
+        Question question = this.questionService.getQuestionById(id);
+        SiteUser siteUser = this.siteUserService.getSiteUser(principal.getName());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "board_detail";
+        }
+
+        Answer answer = this.answerService.createAnswer(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/board/detail/%s#answer_%s", id, answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyAnswer(@PathVariable("id") Integer id, Principal principal, AnswerForm answerForm) {
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!principal.getName().equals(answer.getAuthor().getUsername())) {
+            throw new AccessDeniedException("You do not have permission to access this resource");
+        }
+
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modifyAnswer(@PathVariable("id") Integer id, @Valid AnswerForm answerForm,
+                               BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = answerService.getAnswer(id);
+        if (!principal.getName().equals(answer.getAuthor().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        this.answerService.modifyAnswer(answer, answerForm.getContent());
+        return String.format("redirect:/board/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteAnswer(@PathVariable("id") Integer id, Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        if (!principal.getName().equals(answer.getAuthor().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+        this.answerService.deleteAnswer(answer);
+        return String.format("redirect:/board/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping(value = "/vote/{id}")
+    public String voteAnswer(@PathVariable("id") Integer id, Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        SiteUser siteUser = this.siteUserService.getSiteUser(principal.getName());
+
+        this.answerService.voteAnswer(answer, siteUser);
+        return String.format("redirect:/board/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
+    }
+}
